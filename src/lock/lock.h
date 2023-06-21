ed5226e517bff1dca50569d022c094fe5a628c60
幻想 <jiaxin.cuckoo@gmail.com>
2023-06-21 14:10:20 +0800

    init repo

---
+++ src/lock/lock.h
@@ -0,0 +1,11 @@
+//
+// Created by jiaxin on 2023/6/21 13:58.
+//
+
+#ifndef TINYWEBSERVER_LOCK_H
+#define TINYWEBSERVER_LOCK_H
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#endif //TINYWEBSERVER_LOCK_H
